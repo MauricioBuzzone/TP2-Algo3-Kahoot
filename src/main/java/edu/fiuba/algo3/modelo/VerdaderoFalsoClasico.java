8ca57b984f582e79a661a92a0a7dd29519f71270
MauricioBuzzone <mbuzzone@fi.uba.ar>
2020-07-20 01:28:57 -0300

    feat:Agrego test1 y test2

---
+++ src/main/java/edu/fiuba/algo3/modelo/VerdaderoFalsoClasico.java
@@ -0,0 +1,11 @@
+package edu.fiuba.algo3.modelo;
+
+public class VerdaderoFalsoClasico{
+    private String enunciado;
+    private boolean respuesta;
+
+    public VerdaderoFalsoClasico(String enunciado ,boolean respuesta){
+        this.enunciado = enunciado;
+        this.respuesta = respuesta;
+    }
+}
--- src/main/test/edu/fiuba/algo3/modelo/MessageTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/MessageTest.java
@@ -1,4 +1,6 @@
 package edu.fiuba.algo3.modelo;
+import static org.mockito.Mockito.*;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,5 +11,18 @@ public class MessageTest {
         Message message = new Message("Hello world!");
 
         assertEquals("Hello world!", message.greet());
+    }
+    @Test
+    public void jejeje() {
+        // mock creation
+        List mockedList = mock(List.class);
+
+        // using mock object - it does not tmvnhrow any "unexpected interaction" exception
+        mockedList.add("one");
+        mockedList.clear();
+
+        // selective, explicit, highly readable verification
+        verify(mockedList).add("one");
+        verify(mockedList).clear();
     }
 }
