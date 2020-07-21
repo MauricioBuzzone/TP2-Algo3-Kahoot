001adb7e240384acf7bdfa64c8aa8ec5d028ace2
Lukas DAR <60785752+Lukas-De-Angelis-Riva@users.noreply.github.com>
2020-07-21 16:25:48 -0300

    Agrego test a la clase Respuesta

--- src/main/java/edu/fiuba/algo3/modelo/Respuesta.java
+++ src/main/java/edu/fiuba/algo3/modelo/Respuesta.java
@@ -5,19 +5,19 @@ public class Respuesta {
     private Jugador jugador;
     private Validez validez;
     private Opcion opcion;
-}
 
-public Respuesta(Jugador jugador, Opcion opcion){
-    this.opcion = opcion;
-    this.jugador = jugador;
-    this.validez = null; //hay q crear la clase invalida? o lo dejamos sin inicializar
-}
+    public Respuesta(Jugador jugador, Opcion opcion) {
+        this.opcion = opcion;
+        this.jugador = jugador;
+        this.validez = null; //Por ahora queda así, pero estaría bien refactorizar
+    }
 
-public void evaluarConCriterio(Criterio criterio){
-    validez = criterio.validarCriterio(opcion);
-}
+    public void evaluarConCriterio(Criterio criterio) {
+        validez = criterio.validarCriterio(opcion);
+    }
 
-public void responder(){
-    validez.responder(jugador);
-}
+    public void responder() {
+        validez.responder(jugador);
+    }
 
+}
