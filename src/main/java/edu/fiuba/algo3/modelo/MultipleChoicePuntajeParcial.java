e09e6ef78368cc0e61d520906f67189182eb1fba
Santiago Valdez <santiagoval1999@gmail.com>
2020-07-30 00:02:44 -0300

    doc: pruebas de excepciones

--- src/main/java/edu/fiuba/algo3/modelo/MultipleChoicePuntajeParcial.java
+++ src/main/java/edu/fiuba/algo3/modelo/MultipleChoicePuntajeParcial.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class MultipleChoicePuntajeParcial extends Pregunta {
 
     public MultipleChoicePuntajeParcial(String enunciado, Eleccion eleccion) {
+
+        if(eleccion.cantidadDeOpciones() <= 0 || eleccion.cantidadDeOpciones() > 5){
+            throw new SolucionInvalidaException();
+        }
         this.criterio = new CriterioMultipleChoiceParcial(eleccion);
         this.enunciado = enunciado;
     }
