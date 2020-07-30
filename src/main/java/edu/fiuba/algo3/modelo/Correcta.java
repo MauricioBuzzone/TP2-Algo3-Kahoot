1a15b7e8ce589afb622e259dd35f64f2ded1cdee
MauricioBuzzone <mbuzzone@fi.uba.ar>
2020-07-30 11:55:43 -0300

    Refactor: Excepciones Certificado

--- src/main/java/edu/fiuba/algo3/modelo/Correcta.java
+++ src/main/java/edu/fiuba/algo3/modelo/Correcta.java
@@ -5,6 +5,9 @@ public class Correcta implements Certificado {
     private int puntos;
 
     public Correcta(int puntos){
+        if(puntos < 0){
+            throw new PuntosInvalidosException();
+        }
         this.puntos = puntos;
     }
 
---
+++ src/main/java/edu/fiuba/algo3/modelo/PuntosInvalidosException.java
@@ -0,0 +1,3 @@
+package edu.fiuba.algo3.modelo;
+
+public class PuntosInvalidosException extends RuntimeException{}
