77367b5de970d499f53d5bbc7ea3e13e76962be6
Pedro Aguilar <paguilar@fi.uba.ar>
2020-08-18 11:14:42 -0300

    ref:Vbox verdadero falso y Hbox bonificadores mejorados

--- src/main/java/edu/fiuba/algo3/vista/ContenedorVerdaderoFalso.java
+++ src/main/java/edu/fiuba/algo3/vista/ContenedorVerdaderoFalso.java
@@ -8,6 +8,11 @@ import javafx.scene.layout.VBox;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import java.util.List;
+import javafx.scene.text.Font;
+import java.io.FileInputStream;
+import java.io.IOException;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 
 
 public class ContenedorVerdaderoFalso extends VBox{
@@ -15,16 +20,40 @@ public class ContenedorVerdaderoFalso extends VBox{
     private static final int ESPACIADO_BOTONES = 15;
     private static final String VERDADERO = "Verdadero";
     private static final String FALSO = "Falso";
+    private static final String PATH_IMAGENES_BOTONES = "resources/BotonVerdaderoFalso/";
+    private static final String NOMBRE_IMAGENES_PRIMERA_PARTE = "IMG_BOTON_MC_";
+    private static final String EXTENSION_IMAGENES = ".png";
 
     public ContenedorVerdaderoFalso(ControladorRespuesta controlador){
         super();
         Button botonVerdadero = new Button();
         botonVerdadero.setText(VERDADERO);
+        botonVerdadero.setFont(new Font(App.FUENTE, 20));
         botonVerdadero.setOnAction(new BotonOpcionUnicaEventHandler(controlador, VERDADERO));
+        FileInputStream input1 = null;
+        try {
+            input1 = new FileInputStream(PATH_IMAGENES_BOTONES + NOMBRE_IMAGENES_PRIMERA_PARTE + "1" + EXTENSION_IMAGENES);
+        }catch(IOException ex){
+
+        }
+        Image  imagenBoton1 = new Image(input1);
+        ImageView imageView1 = new ImageView(imagenBoton1);
+        botonVerdadero.setGraphic(imageView1);
 
         Button botonFalso = new Button();
         botonFalso.setText(FALSO);
+        botonFalso.setFont(new Font(App.FUENTE, 20));
         botonFalso.setOnAction(new BotonOpcionUnicaEventHandler(controlador, FALSO));
+        FileInputStream input2 = null;
+        try {
+            input2 = new FileInputStream(PATH_IMAGENES_BOTONES + NOMBRE_IMAGENES_PRIMERA_PARTE + "2" + EXTENSION_IMAGENES);
+        }catch(IOException ex){
+
+        }
+        Image imagenBoton2 = new Image(input2);
+        ImageView imageView2 = new ImageView(imagenBoton2);
+        botonFalso.setGraphic(imageView2);
+
 
         this.getChildren().addAll(botonVerdadero, botonFalso);
         this.setSpacing(ESPACIADO_BOTONES);
